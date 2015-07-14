8f631e5b19743a440b5bbb149ffc48753a4d049b
martin.haesler <martin.haesler@gmail.com>
2015-07-14 16:53:51 +0100

    Implement main function.

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -1,6 +1,7 @@
 // guessinggame
 // guess a random number between 0 - 100.
 
+#include <iostream>
 #include <cstdlib>
 #include <time.h>
 using namespace std;
@@ -13,8 +14,25 @@ int rand_number() {
 int main() {
   int input_var = 0;
   int target = rand_number();
-  cout << "Guess a number between 0 "
+  while (true) {
+    cout << "Guess a number between 0 and 100: ";
+
+    if (!(cin >> input_var ) || input_var > 100 || input_var < 0) {
+      cout << "You need to enter a number between 0 and 100!" << endl;
+      cin.clear();
+      cin.ignore(10000, '\n');
+    }
+    if (input_var > target) {
+      cout << "Your guess is too high!" << endl;
+    } else if (input_var < target) {
+      cout << "Your guess is too low!" << endl;
+    } else if (input_var == target) {
+      cout << "Congratulations, You guessed the correct number!" << endl;
+      return 0;
+    }
   
+  }
+  return -1;
 }
 
 
