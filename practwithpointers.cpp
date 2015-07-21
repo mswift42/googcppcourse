147348cb9fca1233e62d1d092daa41a2e170c220
martin.haesler <martin.haesler@gmail.com>
2015-07-21 20:07:34 +0100

    Add practwithpointers cpp

--- practwithpointers.cpp
+++ practwithpointers.cpp
@@ -1,2 +1,14 @@
 // practicewithpointers.cpp
 
+#include <iostream>
+
+int main() {
+  int my_int = 46;
+
+  int *my_pointer = &my_int;
+  std::cout << my_int << std::endl;
+
+  *my_pointer = 107;
+  std::cout << my_int << std::endl;
+  std::cout << *my_pointer << std::endl;
+}
